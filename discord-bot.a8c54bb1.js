e6b07437b30c89c214366f5d88482973c862c875
mum33n <azeezmumeen6@gmail.com>
2022-02-04 06:56:53 +0100

    initial commit

---
+++ discord-bot.a8c54bb1.js
@@ -0,0 +1,2 @@
+const toggler=document.getElementById("mobile-menu");toggler.addEventListener("click",(function(){toggler.classList.toggle("is-active")}));var wow=new WOW;wow.init();
+//# sourceMappingURL=discord-bot.a8c54bb1.js.map
---
+++ index.2e07e232.js
@@ -0,0 +1,2 @@
+const options={strings:["Molar Finance"],typeSpeed:120},typed=new Typed(".typing",options),toggler=document.getElementById("mobile-menu");toggler.addEventListener("click",(function(){toggler.classList.toggle("is-active")}));var wow=new WOW;wow.init();
+//# sourceMappingURL=index.2e07e232.js.map
